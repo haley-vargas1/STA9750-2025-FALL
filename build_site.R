e586dc33facc71f5d5c246d10934c0190fea77d5
haley-vargas1 <haley.vargas@baruchmail.cuny.edu>
2025-09-09 17:36:35 -0400

    added extra markdown, bolded some words.

--- build_site.R
+++ build_site.R
@@ -6,6 +6,7 @@ library(quarto)
 if(!quarto::quarto_binary_sitrep()){
   stop("Something is wrong with your quarto installation.")
 }
+
 quarto::quarto_render(".")
 system("git add docs/*")
 if(!any(grepl("rstudio", search()))){q("no")}
